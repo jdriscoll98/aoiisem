c64a2b34d287df38aee6c0caee871925929cdf32
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2018-12-01 21:33:05 -0500

    Created Scheduling Models

--- Scheduling/admin.py
+++ Scheduling/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from Scheduling.models import Days, ShiftType, Shift, Availability
+
+admin.site.register(Days)
+admin.site.register(ShiftType)
+admin.site.register(Shift)
+admin.site.register(Availability)
--- Scheduling/models.py
+++ Scheduling/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from House.models import House
+from Employment.models import Employee
+
+class Days(models.Model):
+    day = models.CharField(max_length=8)
+
+class ShiftType(models.Model):
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    number_of_employees = models.IntegerField()
+
+class Shift(models.Model):
+    Type = models.ForeignKey(ShiftType, on_delete=models.CASCADE)
+    up_for_trade = models.BooleanField(default=False)
+    Employee = models.OneToOneField(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+
+class Availability(models.Model):
+    ShiftType = models.ForeignKey(ShiftType, on_delete=models.CASCADE)
+    days = models.ManyToManyField(Days)
