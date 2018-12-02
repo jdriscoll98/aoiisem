9f0c2b5eb9a662b4eff7d30f2ffb44f703c585d9
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2018-12-01 20:57:30 -0500

    Employment Models Created

--- Employment/admin.py
+++ Employment/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from Employment.models import Employee, Manager
+
+admin.site.register(Employee)
+admin.site.register(Manager)
--- Employment/models.py
+++ Employment/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    email = models.EmailField()
+    pay_rate = models.IntegerField()
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
