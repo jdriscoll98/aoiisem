c6ddf548b2f846c2eaddfd8e6c279cc1a87351e2
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2018-12-20 23:37:25 -0500

    accepting applicants

--- Application/models.py
+++ Application/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 class Applicant(models.Model):
     YEAR_IN_SCHOOL_CHOICES = (
@@ -10,6 +11,8 @@ class Applicant(models.Model):
 )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     date_submitted = models.DateField()
     Grade = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
 #    Resume = models.FileField(upload_to='resumes', default='resumes/resume.pdf')
