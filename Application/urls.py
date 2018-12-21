eb22d391a679ff5e3a8a00760306441fc4895c06
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2018-12-20 23:38:10 -0500

    lol

--- Application/urls.py
+++ Application/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.urls import path
 from Application.views import (NewApplication, ApplicationHomePage, UpdateApplication,
- ApplicantDetails, AcceptApplicant)
+ ApplicantDetails, AcceptApplicant, DeleteApplicant)
 # Application Routes (URLs)
 
 app_name = 'Application'
@@ -12,5 +12,6 @@ urlpatterns = [
 		url(r'^UpdateApplication/(?P<pk>\d+)/$', UpdateApplication.as_view(), name='UpdateApplication'),
 		url(r'^ApplicantDetails/(?P<pk>\d+)/$', ApplicantDetails.as_view(), name='ApplicantDetails'),
 		url(r'^AcceptApplicant/(?P<pk>\d+)/$', AcceptApplicant.as_view(), name='AcceptApplicant'),
+		url(r'^DeleteApplicant/(?P<pk>\d+)/$',DeleteApplicant.as_view(), name='DeleteApplicant'),
     	# General Page Views
 ]
