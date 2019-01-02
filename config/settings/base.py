aad94638c08050506e9b8b83dfb22fc1058aed4f
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-01-02 16:27:26 -0500

    ugh

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,11 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Login Settings
 
