ea192c7b9a9c33f4b87c4c8d967a33a750be7163
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-01-06 21:52:33 -0500

    hopefully a fix

--- Employment/views.py
+++ Employment/views.py
@@ -111,7 +111,7 @@ class ViewSchedule(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         Types = ShiftType.objects.all()
-        shifts = Shift.objects.all()[:30]
+        shifts = Shift.objects.all().order_by('pk')
         if len(shifts) >= 30:
             full_schedule = True
         else:
